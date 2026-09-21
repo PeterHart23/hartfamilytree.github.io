cad7fc2601c4bc5e4819d2e865621710047ccc54
Peter Hart <Peter.Hart@sherwin.com>
2026-09-21 18:02:22 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Served at https://peterhart23.github.io/hartfamilytree.github.io/ (a
+  // project site, since the repo name isn't the owner's *.github.io user site).
+  base: '/hartfamilytree.github.io/',
   plugins: [vue()],
 })
